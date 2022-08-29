3bb9850318d32c850e88a2ccb751a8cf2ef26d41
MadeCodeRD <Madelson.Acosta2@gmail.com>
2022-08-29 19:59:57 -0400

    Created the nextjs app

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+const NewMeetupPage = () => {
+
+    const router = useRouter();
+
+     const addMeetupHandler = async (enteredMeetupData) => {
+       const response = await fetch('/api/new-meetup', {
+        method: 'POST',
+        body: JSON.stringify(enteredMeetupData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+       });
+
+       const data = await response.json();
+       console.log(data);
+
+       router.replace('/');
+
+    };
+
+    return (
+        <>
+        <Head>
+        <title>Add a new meet-up</title>
+        <meta name='description' content="Add a meetup" />
+        </Head>
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </>
+    );
+};
+
+export default NewMeetupPage;
